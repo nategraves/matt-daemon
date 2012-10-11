3c4225abbce29c3c310d6da9ccb6a86b520275f1
Adam Detrick <akdetrick@gmail.com>
2012-10-11 16:51:34 -0400

    subscription stubs

--- mattD.rb
+++ mattD.rb
@@ -1,3 +1,4 @@
+require 'active_support'
 require 'cinch'
 require 'yaml'
 
@@ -6,6 +7,9 @@ data = YAML.load_file('quotes.yaml')
 Quotes = data['inspiration']
 Exercises = data['exercises']
 
+# TODO: dump this in yaml so we have it if the pipe breaks
+subscribers = [] 
+
 bot = Cinch::Bot.new do
     configure do |c|
         c.server = "irc.freenode.org"
@@ -13,19 +17,28 @@ bot = Cinch::Bot.new do
         c.channels = ["#cinch-bots"]
     end
 
-    on :message, "hello" do |m|
-        m.reply "Hello, #{m.user.nick}"
+    # subscription
+    on :message, "subscribe" do |m|
+        subscribers << m.user.nick 
+        m.reply "You're in."
+    end
+    on :message, "unsubscribe" do |m|
+        subscribers.delete(m.user.nick)
+        m.reply "You're out."
     end
 
+    # DEBUG quotes
     on :message, "quote" do |m|
         quote = Quotes.sample
         m.reply "\"#{quote}\" - Matt Damon" 
     end
 
+    # DEBUG exercises
     on :message, "exercise" do |m|
+        peeps = subscribers.join(",")
         exercise = Exercises.sample
         reps = 15 # TODO: don't hard code this
-        m.reply "Do #{reps} #{exercise}" 
+        m.reply "#{peeps}\: Do #{reps} #{exercise}!" 
     end
 end
 
