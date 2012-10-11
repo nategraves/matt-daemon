83ae5047a3497ccf18d54306db39f55c269bfa8b
Adam Detrick <akdetrick@gmail.com>
2012-10-11 18:06:53 -0400

    subscription list works offline

--- mattD.rb
+++ mattD.rb
@@ -1,15 +1,32 @@
-require 'active_support'
 require 'cinch'
 require 'yaml'
 
-data = YAML.load_file('quotes.yaml')
-
+# DAMON DATA ------
+data = YAML.load_file('data.yaml')
+reps = 15 # TODO: don't hard code this; increase by 5 every 2 hours or something
 Quotes = data['inspiration']
 Exercises = data['exercises']
 
-# TODO: dump this in yaml so we have it if the pipe breaks
-subscribers = [] 
+# SUBSCRIBER DATA ------
+if File.exist?('subscribers.yaml')
+    subscriberList = YAML.load_file('subscribers.yaml')
+else
+    subscriberList = [] 
+end
 
+# helper methods ------
+def saveSubscribers(list)
+    File.open('subscribers.yaml', 'w+') { |out|
+        YAML::dump(list, out)
+    }
+end
+
+def inspire(m)
+    quote = Quotes.sample
+    m.reply "\"#{quote}\" - Matt Damon" 
+end
+
+# MattDaemon ------
 bot = Cinch::Bot.new do
     configure do |c|
         c.server = "irc.freenode.org"
@@ -17,28 +34,41 @@ bot = Cinch::Bot.new do
         c.channels = ["#cinch-bots"]
     end
 
-    # subscription
     on :message, "subscribe" do |m|
-        subscribers << m.user.nick 
-        m.reply "You're in."
-    end
-    on :message, "unsubscribe" do |m|
-        subscribers.delete(m.user.nick)
-        m.reply "You're out."
+        nick = m.user.nick
+        if subscriberList.include?(nick)
+            m.reply "I like your enthusiasm, #{nick}, but you're already on the list. MattDaemon still loves you."
+        else
+            subscriberList << nick 
+            saveSubscribers(subscriberList)
+            m.reply "#{nick} will be notified of exercise times. MattDaemon loves you."
+        end
     end
 
-    # DEBUG quotes
+    on :message, "unsubscribe" do |m|
+        nick = m.user.nick
+        if subscriberList.include?(nick)
+            subscriberList.delete(nick)
+            saveSubscribers(subscriberList)
+            m.reply "You suck, #{nick}."
+        else
+            m.reply "You're not on the list, #{nick}. You suck."
+        end
+    end
+
+    # on demand inspiration!
     on :message, "quote" do |m|
-        quote = Quotes.sample
-        m.reply "\"#{quote}\" - Matt Damon" 
+        inspire(m)
     end
 
     # DEBUG exercises
     on :message, "exercise" do |m|
-        peeps = subscribers.join(",")
+        peeps = subscriberList.join(",")
         exercise = Exercises.sample
-        reps = 15 # TODO: don't hard code this
-        m.reply "#{peeps}\: Do #{reps} #{exercise}!" 
+        if subscriberList.any?
+            inspire(m)
+            m.reply "#{peeps}\: Do #{reps} #{exercise}!" 
+        end
     end
 end
 
