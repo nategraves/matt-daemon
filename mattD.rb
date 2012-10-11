36f52f6d8bc249c7703854939f48fd86ca6b770a
Adam Detrick <akdetrick@gmail.com>
2012-10-11 15:49:03 -0400

    we now can source yaml for data

--- mattD.rb
+++ mattD.rb
@@ -1,14 +1,31 @@
 require 'cinch'
+require 'yaml'
 
-# hello world
+data = YAML.load_file('quotes.yaml')
+
+Quotes = data['inspiration']
+Exercises = data['exercises']
+
 bot = Cinch::Bot.new do
     configure do |c|
         c.server = "irc.freenode.org"
+        c.nick = "MattDaemon"
         c.channels = ["#cinch-bots"]
     end
 
     on :message, "hello" do |m|
         m.reply "Hello, #{m.user.nick}"
+    end
+
+    on :message, "quote" do |m|
+        quote = Quotes.sample
+        m.reply "\"#{quote}\" - Matt Damon" 
+    end
+
+    on :message, "exercise" do |m|
+        exercise = Exercises.sample
+        reps = 15 # TODO: don't hard code this
+        m.reply "Do #{reps} #{exercise}" 
     end
 end
 
